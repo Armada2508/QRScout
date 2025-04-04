5e0fc5358c3a60bd8f9b2ffa0fdb5ecf4b801a77
Andrew <baumgand000@stillwaterschools.org>
2025-04-04 08:36:51 -0500

    removed duplicate "Played Defense?"

--- QRScoutScanner/ocr.py
+++ QRScoutScanner/ocr.py
@@ -139,7 +139,7 @@ def update_google_sheet(qr_data):
     if sheet_number == 2:
         #print(sheet_number)
         try:
-            sheet = client.open("Reefscape Scouter Spreadsheet UofMN").worksheet("Pit")
+            sheet = client.open("Scouting Data UofMN(Reefscape)").worksheet("Pit")
     
         except gspread.exceptions.SpreadsheetNotFound:
             raise ValueError("Error: Google Sheet 'Reefscape Scouter Spreadsheet' not found. Check the name or share settings.")
@@ -158,7 +158,7 @@ def update_google_sheet(qr_data):
     elif sheet_number == 1:
         #print(sheet_number)
         try:
-            sheet = client.open("Reefscape Scouter Spreadsheet UofMN").worksheet("Match")
+            sheet = client.open("Scouting Data UofMN(Reefscape)").worksheet("Match")
     
         except gspread.exceptions.SpreadsheetNotFound:
             raise ValueError("Error: Google Sheet 'Reefscape Scouter Spreadsheet' not found. Check the name or share settings.")
@@ -177,7 +177,7 @@ def update_google_sheet(qr_data):
     elif sheet_number == 3:
         #print(sheet_number)
         try:
-            sheet = client.open("Reefscape Scouter Spreadsheet UofMN").worksheet("Cycle")
+            sheet = client.open("Scouting Data UofMN(Reefscape)").worksheet("Cycle")
             #print("sheet opened!")
         except gspread.exceptions.SpreadsheetNotFound:
             raise ValueError("Error: Google Sheet 'Reefscape Scouter Spreadsheet' not found. Check the name or share settings.")
